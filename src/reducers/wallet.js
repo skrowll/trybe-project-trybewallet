dd93064b7d0e895f66df83efadd1ee58879bdd4c
Lucas Rocha <lucasdejesus.rocha@hotmail.com>
2022-04-01 01:28:14 -0300

    Requisito 1 OK

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,10 +1,11 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
 
-// const INITIAL_STATE = {
-//   currencies: [],
-//   expenses: [],
-// };
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
 
 // const wallet = (state = INITIAL_STATE, action) => state;
+const wallet = (state = INITIAL_STATE) => state;
 
 export default wallet;
